b82ad6249916867f6c0c5c3deef1e1d2e4b76b04
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-03-12 21:05:06 +0100

    opencv integration mat and save png on disk

---
+++ compML/Assets/_scripts/OpenCVScripts/RenderTextureManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.UnityUtils;
+using System.IO;
+
+public class RenderTextureManager : MonoBehaviour
+{
+
+    public RenderTexture camRenderTexture;
+    public GameObject plane;
+    Texture2D texture;
+
+    private void Update()
+    {
+        //if true, The error log of the Native side OpenCV will be displayed on the Unity Editor Console.
+        Utils.setDebugMode(true);
+
+        Texture2D imgTexture = toTexture2D(camRenderTexture);
+        
+        Mat imgMat = new Mat(imgTexture.height, imgTexture.width, CvType.CV_8UC4);
+
+        Utils.texture2DToMat(imgTexture, imgMat);
+        Debug.Log("imgMat.ToString() " + imgMat.ToString());
+
+        texture = new Texture2D(imgMat.cols(), imgMat.rows(), TextureFormat.RGBA32, false);
+
+        Utils.matToTexture2D(imgMat, texture);
+
+        plane.GetComponent<Renderer>().material.mainTexture = texture;
+        
+        Utils.setDebugMode(false);
+
+    }
+
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new UnityEngine.Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+
+    public void SaveTextureFromButton()
+    {
+        SaveTexturePNG(texture);
+    }
+
+    void SaveTexturePNG(Texture2D tex)
+    {
+        // Encode texture into PNG
+        byte[] bytes = tex.EncodeToPNG();
+        Object.Destroy(tex);
+
+        // For testing purposes, also write to a file in the project folder
+        File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
+    }
+}
