3f67c23367087ec3c251a8ac5716ffd64ca72328
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-03-28 12:16:00 +0100

    set UI. need to fix singleton GAmEMAnaGEr and touch UI intereferences

---
+++ compML/Assets/_scripts/UI_scripts/LoadSceneButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    private Button button;
+    [SerializeField]
+    private int scene;
+
+    static public string username;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TryLoadScene);
+    }
+
+    private void TryLoadScene()
+    {
+        SceneManager.LoadSceneAsync(scene);
+    }
+}
---
+++ compML/Assets/_scripts/UI_scripts/LoadSceneInputUsername.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneInputUsername : MonoBehaviour
+{
+    private TMP_InputField usernameInputField;
+    [SerializeField]
+    private int scene;
+
+    static public string username;
+
+    private void Start()
+    {
+        usernameInputField = GetComponent<TMP_InputField>();
+        usernameInputField.onEndEdit.AddListener(TryLoadScene);
+    }
+
+    private void TryLoadScene(string username)
+    {
+        Debug.Log(username);
+        SceneManager.LoadSceneAsync(scene);
+    }
+}
