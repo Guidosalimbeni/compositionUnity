1d9c2677d7c7701e41b794e2162e393ca4c1ac11
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-04-02 23:28:28 +0100

    added the breeding and breed population funcionality. Still stops at one generation. need to implement next start couroutine

--- compML/Assets/_scripts/Genetic_Algorithm/PopulationManager.cs
+++ compML/Assets/_scripts/Genetic_Algorithm/PopulationManager.cs
@@ -6,23 +6,25 @@ using System.Linq;
 public class PopulationManager : MonoBehaviour {
 
     public bool triggerAI = false;
-
 	public GameObject Individual;
-	public int populationSize = 10;
-	List<GameObject> population = new List<GameObject>();
+    public int populationSize = 10;
+    public int NumberOfGeneration = 4;
 
     private int counterForPopulation = 0;
-    private bool timeForSpawnIsPassed = true;
+    private bool AICreatesPopulationTurn = true; //
     private float secondToWaitForPopGeneration = 0.5f;
+    private List<GameObject> population = new List<GameObject>();
 
     private float trialTime = 5;
-    private float elapsed = 0;
+    private bool startBreeding = false;
     [SerializeField]
     private GameObject elemComp_a;
     [SerializeField]
     private GameObject elemComp_b;
     [SerializeField]
     private GameObject elemComp_c;
+
+    private int generation = 0;
 
 
     private void FixedUpdate()
@@ -34,26 +36,16 @@ public class PopulationManager : MonoBehaviour {
             elemComp_b.GetComponent<ClickToMoveBySelection>().AIisPlaying = true;
             elemComp_c.GetComponent<ClickToMoveBySelection>().AIisPlaying = true;
 
-            if (timeForSpawnIsPassed == true)
+            if (AICreatesPopulationTurn == true)
             {
-                if (counterForPopulation < populationSize)
-                {
-                    StartCoroutine(CreatePopulation());
-                    
-                    counterForPopulation++;
-                }
-                else
-                {
-                    counterForPopulation = 0;
-                }
+                StartCoroutine(CreatePopulation());
             }
 
-            elapsed += Time.deltaTime;
-
-            if (elapsed > trialTime)
+            if (startBreeding == true)
             {
                 BreedNewPopulation();
-                elapsed = 0;
+                //StartCoroutine(BreedNewPopulation()); // TODO
+                startBreeding = false;
             }
         }
 
@@ -70,25 +62,35 @@ public class PopulationManager : MonoBehaviour {
 
     private IEnumerator CreatePopulation()
     {
-        timeForSpawnIsPassed = false;
+        AICreatesPopulationTurn = false;
         GameObject IndividualCompositionSet = Instantiate(Individual, this.transform.position, this.transform.rotation);
         IndividualCompositionSet.GetComponent<Brain>().Init();
         population.Add(IndividualCompositionSet);
         yield return new WaitForSeconds(secondToWaitForPopGeneration);
-        timeForSpawnIsPassed = true;
-
-
+        
+        if (counterForPopulation < populationSize)
+        {
+            startBreeding = false;
+            AICreatesPopulationTurn = true;
+            counterForPopulation++;
+        }
+        else
+        {
+            AICreatesPopulationTurn = false;
+            counterForPopulation = 0;
+            startBreeding = true;
+        }
     }
 
 
     void BreedNewPopulation()
     {
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().TotalScore).ToList();
 
-        Debug.Log("breeeedddd");
-        /*
-        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<Brain>().timeAlive).ToList();
+        //Debug.Log(sortedList[0]);
 
         population.Clear();
+        
         for (int i = (int)(sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
         {
             population.Add(Breed(sortedList[i], sortedList[i + 1]));
@@ -100,19 +102,16 @@ public class PopulationManager : MonoBehaviour {
             Destroy(sortedList[i]);
         }
         generation++;
-        */
+        Debug.Log(generation);
+        
     }
 
-
-
-    /*
+    
     GameObject Breed(GameObject parent1, GameObject parent2)
 	{
-        
-		Vector3 startingPos = new Vector3(this.transform.position.x + Random.Range(-2,2),
-												this.transform.position.y,
-												this.transform.position.z + Random.Range(-2,2));
-		GameObject offspring = Instantiate(botPrefab, startingPos, this.transform.rotation);
+
+        GameObject offspring = Instantiate(Individual, this.transform.position, this.transform.rotation);
+
 		Brain b = offspring.GetComponent<Brain>();
 		if(Random.Range(0,100) == 1) //mutate 1 in 100
 		{
@@ -124,10 +123,13 @@ public class PopulationManager : MonoBehaviour {
 			b.Init();
 			b.dna.Combine(parent1.GetComponent<Brain>().dna,parent2.GetComponent<Brain>().dna);
 		}
+
+        Debug.Log("offspring generated");
 		return offspring;
+
     }
-        
-     */
+
+
 
 }
 
