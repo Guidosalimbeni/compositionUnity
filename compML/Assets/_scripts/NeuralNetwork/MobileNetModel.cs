7e239cc96c9207627114226e6263c9892518e923
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-07-13 12:14:51 +0100

    implemented union android classifier github

--- compML/Assets/_scripts/NeuralNetwork/MobileNetModel.cs
+++ compML/Assets/_scripts/NeuralNetwork/MobileNetModel.cs
@@ -1,59 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MLAgents;
-using Barracuda;
+//using MLAgents;
+//using Barracuda;
 
 public class MobileNetModel : MonoBehaviour
 {
-    [SerializeField]
-    private CompositionImage CompImage224;
+    //[SerializeField]
+    //private CompositionImage CompImage224;
 
     
-    private CollectCompositionImage collectCompositionImage;
+    //private CollectCompositionImage collectCompositionImage;
 
-    private IWorker worker;
+    //private IWorker worker;
 
-    // https://github.com/Unity-Technologies/ml-agents/blob/e3b86a27a2547cdd177bf9e848b4337aa71b887a/UnitySDK/Assets/ML-Agents/Plugins/Barracuda.Core/Barracuda.md
-
-
-    private void Start()
-    {
-        bool verbose = false;
-        string modelName = "ModelMorandi_all";
-        Model model = ModelLoader.LoadFromStreamingAssets(modelName + ".bytes", verbose);
-        worker = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.Compute, model);
-
-        collectCompositionImage = FindObjectOfType<CollectCompositionImage>();
-    }
+    //// https://github.com/Unity-Technologies/ml-agents/blob/master/UnitySDK/Assets/ML-Agents/Plugins/Barracuda.Core/Barracuda.md
 
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            UseTexture();
-        }
-    }
+    //public NNModel modelSource;
 
-    private void UseTexture()
-    {
-        collectCompositionImage.AssignCompRenderToTexture();
-        // you can treat input pixels as 1 (grayscale), 3 (color) or 4 (color with alpha) channels
-        Texture2D renderView224MobileNet = CompImage224.CompImage;
+    //private void Start()
+    //{
+    //    //bool verbose = false;
+    //    //string modelName = "ModelMorandi_all";
+    //    //Model model = ModelLoader.LoadFromStreamingAssets(modelName + ".bytes", verbose);  // not used
+        
+    //    var model = ModelLoader.Load(modelSource);
+    //    worker = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.ComputePrecompiled, model);
 
-        int channels = 1;
-        Tensor tensor = new Tensor(renderView224MobileNet, channels);
-        worker.Execute(tensor);
-        Tensor outputMobileNet = worker.Fetch();
+    //    collectCompositionImage = FindObjectOfType<CollectCompositionImage>();
+    //}
 
 
-        Debug.Log("this is the ouput of the MobileNEt    " + outputMobileNet);
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        UseTexture();
+    //    }
+    //}
 
-        outputMobileNet.Dispose();
-        worker.Dispose();
+    //private void UseTexture()
+    //{
+
+    //    collectCompositionImage.AssignCompRenderToTexture();
+    //    // you can treat input pixels as 1 (grayscale), 3 (color) or 4 (color with alpha) channels
+    //    Texture2D renderView224MobileNet = CompImage224.CompImage;
+
+    //    Debug.Log(renderView224MobileNet.format);
+    //    Debug.Log(renderView224MobileNet.width);
+        
+    //    Tensor tensor = new Tensor(renderView224MobileNet);
+    //    worker.Execute(tensor);
+    //    Tensor outputMobileNet = worker.Fetch();
 
 
-    }
+    //    Debug.Log("this is the ouput of the MobileNEt    " + outputMobileNet);
+
+    //    outputMobileNet.Dispose();
+    //    worker.Dispose();
+
+
+    //}
 
 }
