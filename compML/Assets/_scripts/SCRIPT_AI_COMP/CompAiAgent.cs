07768010d3ae542e964c546260d2d0ebb49a970e
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-07-18 19:22:34 +0100

    working on AI

--- compML/Assets/_scripts/SCRIPT_AI_COMP/CompAiAgent.cs
+++ compML/Assets/_scripts/SCRIPT_AI_COMP/CompAiAgent.cs
@@ -7,9 +7,20 @@ public class CompAiAgent : Agent
 {
     // https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Learning-Environment-Design-Agents.md
 
+    [Header("Specific to CompAI")]
+    private CompAiAcademy academy;
+    public float timeBetweenDecisionsAtInference;
+    private float timeSinceDecision;
+
+    [Tooltip("Because we want an observation right before making a decision, we can force " +
+             "a camera to render before making a decision. Place the agentCam here if using " +
+             "RenderTexture as observations.")]
+    public Camera renderCamera;
+
+
     public override void InitializeAgent()
     {
-        
+        academy = FindObjectOfType(typeof(CompAiAcademy)) as CompAiAcademy;
     }
 
     public override void CollectObservations()
@@ -19,7 +30,47 @@ public class CompAiAgent : Agent
 
     }
 
+    // to be implemented by the developer
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+
+
+
 
 
 
     }
+
+
+    public void FixedUpdate()
+    {
+        WaitTimeInference();
+    }
+
+    private void WaitTimeInference()
+    {
+        if (renderCamera != null)
+        {
+            renderCamera.Render();
+        }
+
+        if (!academy.GetIsInference())
+        {
+            RequestDecision();
+        }
+        else
+        {
+            if (timeSinceDecision >= timeBetweenDecisionsAtInference)
+            {
+                timeSinceDecision = 0f;
+                RequestDecision();
+            }
+            else
+            {
+                timeSinceDecision += Time.fixedDeltaTime;
+            }
+        }
+    }
+
+
+}
