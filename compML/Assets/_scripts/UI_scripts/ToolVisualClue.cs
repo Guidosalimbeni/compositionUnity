ab9aca61982a11c1bbfae5109cb95df6c1eb3ec7
Guidosalimbeni <guido.salimbeni@gmail.com>
2019-07-21 20:45:23 +0100

    implemente visual clue rule of third spiral

---
+++ compML/Assets/_scripts/UI_scripts/ToolVisualClue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolVisualClue : MonoBehaviour
+{
+    public Image Spiral;
+    public Image RuleOfThird;
+
+    public void TurnOnOffSpiral()
+    {
+        Spiral.enabled = !Spiral.enabled;
+    }
+
+    public void TurnOnOffRuleOfThird()
+    {
+        RuleOfThird.enabled = !RuleOfThird.enabled;
+    }
+}
